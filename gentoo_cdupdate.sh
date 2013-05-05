52a6f51019af7606ff903f018ca8c5187e39c430
Christian Nilsson <nikize@gmail.com>
2013-05-05 07:06:28 +0200

    Fix boot network checks. The squashfs needs to be modified to fix PredictableNetworkInterfaceNames This also fixes the documentation and auto detection that is currently broken on the LiveCD

--- gentoo_cdupdate.sh
+++ gentoo_cdupdate.sh
@@ -1,6 +1,4 @@
-# Try to get rid of the PredictableNetworkInterfaceNames Shit! With it we never know what the nics are called.
-rm /lib64/udev/rules.d/80-net-name-slot.rules
-
+setterm -blank 0
 cat >> /newroot/root/.bashrc << EOF
 if [ "\$(hostname)" == "livecd" ] && (tty | grep -q tty1\\$)
 then
@@ -8,7 +6,10 @@ then
   ip link set eth0 up && sleep 2
   dhcpcd
   # wait a bit for everything to come up, and oh the (Un)PredictableNetworkInterfaceNames Madness will make sure eth0 above no longer works.
-  sleep 4
+  while : ; do
+    ping -c 1 raw.github.com && break
+    sleep 2
+  done
   wget https://raw.github.com/ASoft-se/Gentoo-HAI/master/install.sh -O g-install.sh
   echo We just downloaded g-install.sh that can be used to make a install...
   echo maybe fix somethings like passwd before continue?
--- gentoocd_unpack.sh
+++ gentoocd_unpack.sh
@@ -4,31 +4,39 @@ echo emerge -uv1 cdrtools squashfs-tools
 modprobe loop
 mount -r -o loop,users install-amd64-minimal-*.iso /mnt/cdrom || exit 1
 [ ! -d gentoo_boot_cd ] && (mkdir gentoo_boot_cd || exit 1)
+echo Make all changes in a tmpfs for performance, and saving on SSD writes.
+mount none -t tmpfs gentoo_boot_cd -o size=1G,nr_inodes=1048576
 cp -av /mnt/cdrom/* gentoo_boot_cd || exit 1
 umount /mnt/cdrom || exit 1
 
 cd gentoo_boot_cd || exit 1
-#unsquashfs image.squashfs || exit 1
-#rm image.squashfs
+unsquashfs image.squashfs || exit 1
+rm image.squashfs
 # mv squashfs-root ~/squashroot
 
 
 echo do changes...
+# Try to get rid of the PredictableNetworkInterfaceNames Shit! With it we never know what the nics are called.
+echo > squashfs-root/lib64/udev/rules.d/80-net-name-slot.rules
 
 cp ../gentoo_cdupdate.sh cdupdate.sh
 chmod a+x cdupdate.sh
 sed -i 's/ontimeout localhost/ontimeout gentoo/' isolinux/isolinux.cfg
 sed -i 's/vga=791$/vga=791 keymap=32 autoinstall/' isolinux/isolinux.cfg
 
+echo Giving user possibility to modify boot settings
 mcedit isolinux/isolinux.cfg
+echo -e "\n\tStarting separate shell, just exit if no changes should be done."
+echo -e "\n\tWhen exit, the iso will be rebuilt."
 bash
 
-#mksquashfs squashfs-root image.squashfs || exit 1
-#rm -rf squashfs-root
+mksquashfs squashfs-root image.squashfs || exit 1
+rm -rf squashfs-root
 cd ..
 mkisofs -R -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 \
  -hide-rr-moved -c isolinux/boot.cat -o install-amd64-mod.iso gentoo_boot_cd || exit 1
 
+umount gentoo_boot_cd
 rm -rf gentoo_boot_cd
 
 #something else if we use grub...
