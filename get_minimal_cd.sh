183e8bec771de0ba838c277724e4cf28b636ecaa
Christian Nilsson <nikize@gmail.com>
2013-12-27 16:41:19 +0100

    Continue minimal iso download instead Mainly to avoid multiple files.

--- get_minimal_cd.sh
+++ get_minimal_cd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FILE=$(wget -q http://distfiles.gentoo.org/releases/amd64/current-iso/ -O - | grep -o -e "install-amd64-minimal-\w*.iso" | uniq)
 
-wget http://distfiles.gentoo.org/releases/amd64/current-iso/$FILE || exit 1
+wget -c http://distfiles.gentoo.org/releases/amd64/current-iso/$FILE || exit 1
