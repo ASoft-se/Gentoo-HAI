88c6648b372d186cec409878580e7363a8df97b8
Christian Nilsson <nikize@gmail.com>
2015-09-20 15:17:12 +0200

    make iso modifying script handle root case and dont use loop mount

--- gentoocd_unpack.sh
+++ gentoocd_unpack.sh
@@ -1,34 +1,41 @@
-echo To much needing root :\(
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root, please provide password to su" 1>&2
+  su -c "sh $0"
+  exit
+fi
+echo emerge -uv1 p7zip cdrtools squashfs-tools
 set -x
-echo emerge -uv1 cdrtools squashfs-tools
-modprobe loop
-mount -r -o loop,users install-amd64-minimal-*.iso /mnt/cdrom || exit 1
 [ ! -d gentoo_boot_cd ] && (mkdir gentoo_boot_cd || exit 1)
 echo Make all changes in a tmpfs for performance, and saving on SSD writes.
 mount none -t tmpfs gentoo_boot_cd -o size=2G,nr_inodes=1048576
-cp -av /mnt/cdrom/* gentoo_boot_cd || exit 1
-umount /mnt/cdrom || exit 1
-
 cd gentoo_boot_cd || exit 1
+# BUG error if there is multiple matching filenames
+7z x ../install-amd64-minimal-*.iso || exit 1
+
 unsquashfs image.squashfs || exit 1
 rm image.squashfs
 # mv squashfs-root ~/squashroot
 
 
 echo do changes...
-# Try to get rid of the PredictableNetworkInterfaceNames Shit! With it we never know what the nics are called.
+# Try to get rid of the PredictableNetworkInterfaceNames unpredicatability With it we never know what the nics are called.
 echo > squashfs-root/lib64/udev/rules.d/80-net-name-slot.rules
 echo > squashfs-root/lib64/udev/rules.d/80-net-setup-link.rules
 
 cp ../gentoo_cdupdate.sh cdupdate.sh
 chmod a+x cdupdate.sh
+# Change the default to gentoo instead of local
 sed -i 's/ontimeout localhost/ontimeout gentoo/' isolinux/isolinux.cfg
-sed -i 's/vga=791$/vga=791 keymap=32 autoinstall/' isolinux/isolinux.cfg
+# remove do keymap
+sed -i 's/ dokeymap / /' isolinux/isolinux.cfg
+# default to swedish keyboard and add autoinstall TODO make it settable
+sed -i 's/vga=791$/vga=791 keymap=se autoinstall/' isolinux/isolinux.cfg
 
 echo Giving user possibility to modify boot settings
 mcedit isolinux/isolinux.cfg
-echo -e "\n\tStarting separate shell, just exit if no changes should be done."
-echo -e "\n\tWhen exit, the iso will be rebuilt."
+# TODO color ths to make it readable
+echo -e "\n\tStarting separate shell, just exit if no changes should be done.\n\n\tWhen exit, the iso will be rebuilt."
 bash
 
 mksquashfs squashfs-root image.squashfs || exit 1
