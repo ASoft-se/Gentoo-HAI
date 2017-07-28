f8f8fea85a84c899c9cd9fc458f2770ea019c183
Christian Nilsson <nikize@gmail.com>
2017-07-28 15:31:44 +0200

    add bind-tools which provides the dig command, fix #7

--- install.sh
+++ install.sh
@@ -240,7 +240,7 @@ etc-update --automode -5
 
 [ -f /etc/portage/package.mask/gentoo.conf ] || cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
 
-time emerge -uv -j8 gentoo-sources mlocate postfix iproute2 bind quagga dhcp atftp dhcpcd app-misc/mc pciutils usbutils smartmontools syslog-ng vixie-cron ntp lsof || bash
+time emerge -uv -j8 gentoo-sources mlocate postfix iproute2 bind bind-tools quagga dhcp atftp dhcpcd app-misc/mc pciutils usbutils smartmontools syslog-ng vixie-cron ntp lsof || bash
 mkdir /tftproot
 # reinstall eudev, TODO detect if we did switch above and only install if needed
 time emerge -uvN -j8 eudev
