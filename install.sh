a15357ab0d3fadf487ee3b3e9b4f16ca012831ac
Christian I. Nilsson <nikize@gmail.com>
2024-08-14 01:02:04 +0200

    Add memtest86+
    Relates to #9

--- install.sh
+++ install.sh
@@ -299,7 +299,7 @@ etc-update --automode -5
 
 [ -f /etc/portage/package.mask/gentoo.conf ] || cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
 
-time emerge -uv -j8 installkernel grub gentoo-sources pciutils usbutils ntp iproute2 ${NVMETOOLS} || bash
+time emerge -uv -j8 installkernel grub gentoo-sources pciutils usbutils ntp iproute2 sys-apps/memtest86+ ${NVMETOOLS} || bash
 mkdir /tftproot
 lspci
 ntpdate ntp.se
