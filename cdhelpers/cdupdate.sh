40032f7ce2f5297ced5dd62f357192f9297b63ed
Christian I. Nilsson <nikize@gmail.com>
2026-08-11 13:46:32 +0200

    Move copy logic from gentoo_cd_bashrc_addon to cdupdate.sh to support PXE boot
    PXE boot does not have the files in /mnt/cdrom, and must be catched before they are lost in pivot_root
    
    Relates to #49

--- cdhelpers/cdupdate.sh
+++ cdhelpers/cdupdate.sh
@@ -3,9 +3,10 @@
 echo "  Running cdupdate.sh in $(pwd)"
 echo "  Started as: $0 args: $*"
 
-ip a
 scriptpath=$(dirname $0)
 echo " found scriptpath: ${scriptpath}"
 
 echo " Update bashrc with gentoo_cd_bashrc_addon contents ..."
 cat ${scriptpath}/gentoo_cd_bashrc_addon >> root/.bashrc
+[ ! -f ${scriptpath}/g-install.sh ] || cp -v ${scriptpath}/g-install.sh root
+[ ! -f ${scriptpath}/portagehelper.sh ] || cp -v ${scriptpath}/portagehelper.sh root
