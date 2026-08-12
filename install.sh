9c74e1a6151e13453ff91d055eceea5c6b8462ce
Christian I. Nilsson <nikize@gmail.com>
2026-08-12 18:39:08 +0200

    Guestimate number of lines needed to compile the kernel instead of static value

--- install.sh
+++ install.sh
@@ -763,8 +763,8 @@ grub-install --target=i386-pc ${IDEV}
 make_kernel() {
 # 6000 is rough estimate of how many lines we usually get for a compile --dry-run unfortunatly failed
   time (
-    pv -lbtpef -s 6000 > /dev/null < <(make -j$(($(nproc)*2)) bzImage modules) && \
-    pv -lbtpef -s $((10+$(find -name "*.ko" | wc -l))) > /dev/null < <(make -j2 modules_install install)
+    pv -lbtpef -s $(bc <<< "scale=0; ($(grep -c "=[ym]" .config) * 28) / 10") > /dev/null < <(make -j$(($(nproc)*2)) bzImage modules) && \
+    pv -lbtpef -s $((6+$(find -name "*.ko" | wc -l))) > /dev/null < <(make -j2 modules_install install)
     ) || bash
 rm /boot/vmlinuz.old
 ls -lh /boot
