846c885c13e1559987b6a54bd2a2cca29060d920
Christian I. Nilsson <nikize@gmail.com>
2026-08-23 23:36:12 +0200

    Refactor list_latest.sh with the new latest files on the mirrors making it super easy

--- list_latest.sh
+++ list_latest.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 DISTMIRROR=http://distfiles.gentoo.org
-DISTBASE=${DISTMIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/
-FILES=$(wget -q ${DISTBASE} -O - | grep -o -E "\w*-[-0-9A-Za-z\.]*\.iso" | sort -r | head -1)
-FILE_MINIMAL_ISO=$(echo ${FILES} | grep -o -e "install-amd64-minimal-\w*.iso")
+BUILDBASE=${DISTMIRROR}/releases/amd64/autobuilds/
+FILE_MINIMAL_ISO=$(curl -sL ${BUILDBASE}latest-install-amd64-minimal.txt | grep -o -E "\w*-[-0-9A-Za-z\.]*\.iso")
 echo Latest Minimal: $FILE_MINIMAL_ISO
-STAGEBASE=${DISTMIRROR}/releases/amd64/autobuilds/current-stage3-amd64-openrc/
-FILE_STAGE3=$(wget -q ${STAGEBASE} -O - | grep -o -E "stage3-amd64-openrc-\w*\.tar\.xz" | sort -r | head -1)
+FILE_STAGE3=$(curl -sL ${BUILDBASE}latest-stage3-amd64-openrc.txt | grep -o -E "\w*-[-0-9A-Za-z\.]*\.tar\.xz")
 echo Latest Stage3: $FILE_STAGE3
